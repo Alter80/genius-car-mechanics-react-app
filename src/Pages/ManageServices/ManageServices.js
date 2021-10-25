cd4ca629def147c48924e79aa09c6804ce7983e8
Alter80 <fahimatul8@Gmail.com>
2021-10-25 08:35:43 +0600

    first commit

---
+++ src/Pages/ManageServices/ManageServices.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+const ManageServices = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services/')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+
+    //DELETING
+    const handleDelete = id => {
+        const url = `http://localhost:5000/services/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
+
+    return (
+        <div>
+            <h2>This is Manage Services</h2>
+            {
+                services.map(service => <div key={service._id}
+                >
+                    <h3>{service.name}</h3>
+                    <button onClick={() => handleDelete(service._id)}>Delete</button>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default ManageServices;
